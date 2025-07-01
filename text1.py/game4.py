6cf12110bc7bc27c5b0ba015a39ef9d03015849a
robot-1cyy <2014815392@qq.com>
2025-07-01 19:40:55 +0800

    robot-ccyy

---
+++ text1.py/game4.py
@@ -0,0 +1,46 @@
+###数###
+wryin=5+2
+print(wryin)  #加法
+
+aaa=8-1
+print(aaa)  #减法
+
+bbb=3*9
+print(bbb)  #乘法
+
+ccc=99/3
+print(ccc)  #除法
+
+ai=3**5
+print(ai)  #乘方
+
+love=(3+8)*6
+print(love)  #括号
+#浮点数
+you=0.2+0.3
+print(you)
+
+i=0.3*0.2
+print(i)
+#数中下划线
+topy=1_000_000_000
+print(topy)  #python会忽略下划线
+#给多个变量赋值
+a,s,d=1,4,6
+print(a)
+print(s)
+print(d)
+print(a,s,d)
+
+###练习###
+#练习1#
+print(8+6)
+print(7+7)
+print(9+5)
+print(4+10)
+#练习2#
+f_number=666
+message=f"my fevorite number is {f_number}"
+print(message)
+
+
